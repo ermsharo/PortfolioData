b86f0368f99f1fce33866a3efbc42a822a7f7872
Emilio Haro <ermsharo@gmail.com>
2023-08-24 03:32:38 -0300

    update middleware again

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,6 +2,13 @@ module.exports = [
   'strapi::errors',
   'strapi::security',
   'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      poweredBy: 'Some awesome company',
+      origin: '*'
+    },
+  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
